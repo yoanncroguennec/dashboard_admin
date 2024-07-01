8c6449c460342b8411aa1ca1bb8eb97a5f3f0fc4
sido69 <yoann.croguennec@gmail.com>
2024-07-01 18:07:27 +0200

    first commit

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,7 +4,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("/api/topics", {
+    const res = await fetch("http://localhost:3000/api/topics", {
       cache: "no-store",
     });
 
@@ -26,14 +26,14 @@ export default async function TopicsList() {
       {topics.map((t) => (
         <div
           key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+          className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'
         >
           <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <h2 className='font-bold text-2xl'>{t.title}</h2>
             <div>{t.description}</div>
           </div>
 
-          <div className="flex gap-2">
+          <div className='flex gap-2'>
             <RemoveBtn id={t._id} />
             <Link href={`/editTopic/${t._id}`}>
               <HiPencilAlt size={24} />
