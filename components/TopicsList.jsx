d2da77d54321b1064d3e4ea15bbc759beabdc029
sido69 <yoann.croguennec@gmail.com>
2024-07-01 18:18:36 +0200

    first commit

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,7 +1,7 @@
 
 const getTopics = async () => {
   try {
-    const res = await fetch("https://dashboard-admin-muqtkmyzt-yoann-croguennec.vercel.app/api/topics", {
+    const res = await fetch("/api/topics", {
       cache: "no-store",
     });
 
